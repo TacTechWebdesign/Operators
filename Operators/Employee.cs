a069f2bf6c24c72ae142a7e9845c865d6e9e5c6c
TacTechWebdesign <118140620+TacTechWebdesign@users.noreply.github.com>
2023-04-02 10:33:46 +0800

    Add project files.

---
+++ Operators/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+     public class Employee
+
+    {
+        //Create an Employee class with Id, FirstName and LastName properties.
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        public Employee(int id, string fn, string ln)
+        {
+            Id = id;
+            FirstName = fn;
+            LastName = ln;
+        }
+
+        //The comparison operators must be overloaded in pairs.
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Employee employee;
+        }
+    }
+}
---
+++ Operators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(20040, "Rica", "Reyes");
+            Employee emp2 = new Employee(20040, "Selena", "Gomez");
+            Employee emp3 = new Employee(20041, "Haile", "Bebir");
+
+            Console.WriteLine("Are {0} {1} and {2} {3} have the same ID? \n" + (emp1 == emp2), emp1.FirstName, emp1.LastName, emp2.FirstName, emp2.LastName);
+            Console.ReadLine();
+
+            Console.WriteLine("Are {0} {1} and {2} {3} have the same ID? \n" + (emp1 == emp3), emp1.FirstName, emp1.LastName, emp3.FirstName, emp3.LastName);
+            Console.ReadLine();
+
+            Console.WriteLine("Are {0} {1} and {2} {3} different ID? \n" + (emp2 != emp3), emp2.FirstName, emp2.LastName, emp3.FirstName, emp3.LastName);
+            Console.ReadLine();
+        }
+    }
+}
